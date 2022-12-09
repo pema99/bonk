5363f78e34a0eb36b97354cc2d5e3a75fe0caf9c
Pema Malling <pemamalling@gmail.com>
2022-12-09 23:29:31 +0100

    Poor mans test suite

---
+++ examples/aoc2022/aoc_test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+> out.txt
+dotnet run compile examples/aoc2022/day1/day1.bonk && bun out.js >> out.txt
+dotnet run compile examples/aoc2022/day2/day2.bonk && bun out.js >> out.txt
+dotnet run compile examples/aoc2022/day3/day3.bonk && bun out.js >> out.txt
+dotnet run compile examples/aoc2022/day4/day4.bonk && bun out.js >> out.txt
+dotnet run compile examples/aoc2022/day5/day5.bonk && bun out.js >> out.txt
+dotnet run compile examples/aoc2022/day6/day6.bonk && bun out.js >> out.txt
+dotnet run compile examples/aoc2022/day7/day7.bonk && bun out.js >> out.txt
+dotnet run compile examples/aoc2022/day8/day8.bonk && bun out.js >> out.txt
