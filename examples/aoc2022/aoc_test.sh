d386332dda426c77b5c2a01f00550c77b667276b
Pema Malling <pemamalling@gmail.com>
2022-12-15 00:57:59 +0100

    Use special instrincis for aoc

--- examples/aoc2022/aoc_test.sh
+++ examples/aoc2022/aoc_test.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 > out.txt
-dotnet run compile examples/aoc2022/day1/day1.bonk && bun out.js >> out.txt
-dotnet run compile examples/aoc2022/day2/day2.bonk && bun out.js >> out.txt
-dotnet run compile examples/aoc2022/day3/day3.bonk && bun out.js >> out.txt
-dotnet run compile examples/aoc2022/day4/day4.bonk && bun out.js >> out.txt
-dotnet run compile examples/aoc2022/day5/day5.bonk && bun out.js >> out.txt
-dotnet run compile examples/aoc2022/day6/day6.bonk && bun out.js >> out.txt
-dotnet run compile examples/aoc2022/day7/day7.bonk && bun out.js >> out.txt
-dotnet run compile examples/aoc2022/day8/day8.bonk && bun out.js >> out.txt
-dotnet run compile examples/aoc2022/day9/day9.bonk && bun out.js >> out.txt
-dotnet run compile examples/aoc2022/day10/day10.bonk && bun out.js >> out.txt
+dotnet run compile lib/bonk/prelude_js.bonk examples/aoc2022/day1/day1.bonk   && bun out.js >> out.txt
+dotnet run compile lib/bonk/prelude_js.bonk examples/aoc2022/day2/day2.bonk   && bun out.js >> out.txt
+dotnet run compile lib/bonk/prelude_js.bonk examples/aoc2022/day3/day3.bonk   && bun out.js >> out.txt
+dotnet run compile lib/bonk/prelude_js.bonk examples/aoc2022/day4/day4.bonk   && bun out.js >> out.txt
+dotnet run compile lib/bonk/prelude_js.bonk examples/aoc2022/day5/day5.bonk   && bun out.js >> out.txt
+dotnet run compile lib/bonk/prelude_js.bonk examples/aoc2022/day6/day6.bonk   && bun out.js >> out.txt
+dotnet run compile lib/bonk/prelude_js.bonk examples/aoc2022/day7/day7.bonk   && bun out.js >> out.txt
+dotnet run compile lib/bonk/prelude_js.bonk examples/aoc2022/day8/day8.bonk   && bun out.js >> out.txt
+dotnet run compile lib/bonk/prelude_js.bonk examples/aoc2022/day9/day9.bonk   && bun out.js >> out.txt
+dotnet run compile lib/bonk/prelude_js.bonk examples/aoc2022/day10/day10.bonk && bun out.js >> out.txt
+dotnet run compile lib/bonk/prelude_js.bonk examples/aoc2022/day11/day11.bonk && bun out.js >> out.txt
+dotnet run compile lib/bonk/prelude_js.bonk examples/data_structures.bonk examples/aoc2022/day12/day12.bonk && bun out.js >> out.txt
+dotnet run compile lib/bonk/prelude_js.bonk examples/data_structures.bonk examples/aoc2022/day13/day13.bonk && bun out.js >> out.txt
