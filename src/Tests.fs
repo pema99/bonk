f576569ba63a17e70958cb34a253a29d76227c57
pema99 <pemamalling@gmail.com>
2022-12-30 13:01:19 +0100

    JS Tests

--- src/Tests.fs
+++ src/Tests.fs
@@ -10,7 +10,10 @@ open Monad
 open Pretty
 
 open System.IO
+open System.Diagnostics
+open System.Runtime.InteropServices
 open System
+open Pipeline
 
 let compareOrBless name content =
     let path = "tests/output/" + name + ".expected"
@@ -32,6 +35,51 @@ let compareOrBless name content =
             ignore <| Directory.CreateDirectory "tests/output"
         File.WriteAllLines(path, content)
         Ok ()
+
+let fileExists filename =
+    if File.Exists filename then true
+    else
+        let values = Environment.GetEnvironmentVariable "PATH"
+        values.Split Path.PathSeparator
+        |> Seq.exists(fun path -> File.Exists(Path.Combine (path, filename)))
+
+let javaScriptRunner =
+    ["bun"; "node"; "bun.exe"; "node.exe"; "nodejs.exe"]
+    |> Seq.find fileExists
+
+let testJS prelude file =
+    // Set up process
+    use cmd = new Process()
+    if RuntimeInformation.IsOSPlatform OSPlatform.Linux then
+        cmd.StartInfo.FileName <- "/bin/sh"
+    else
+        cmd.StartInfo.FileName <- "cmd.exe"
+    cmd.StartInfo.RedirectStandardInput <- true
+    cmd.StartInfo.RedirectStandardOutput <- true
+    cmd.StartInfo.RedirectStandardError <- true
+    cmd.StartInfo.CreateNoWindow <- true
+    cmd.StartInfo.UseShellExecute <- false
+    cmd.Start() |> ignore
+    // Compile program
+    File.Delete "out.js"
+    let inputPath = "tests/" + file + ".bonk"
+    use sw = new StringWriter()
+    let old = Console.Out
+    Console.SetOut(sw)
+    startCompile prelude "out.js" [inputPath]
+    Console.SetOut(old)
+    // Run program
+    cmd.StandardInput.WriteLine($"{javaScriptRunner} out.js")
+    cmd.StandardInput.Flush()
+    cmd.StandardInput.Close()
+    cmd.WaitForExit()
+    // Compare output
+    let stdout = cmd.StandardOutput.ReadToEnd()
+    let err = sw.GetStringBuilder().ToString()
+    let split =
+        $"{stdout}\n{err}".Split([|"\r\n"; "\n"; "\r"|], StringSplitOptions.None)
+        |> Seq.toList
+    compareOrBless (file + "_compiled") (split)
 
 let listTypes = repl {
     let! ((typeEnv, _, _, _), _, termEnv) = get
@@ -87,7 +135,7 @@ let testPrelude() =
     match runReplAction true action with
     | Ok s -> compareOrBless "prelude" s
     | _ -> Error "Failed to run REPL action."
-    
+
 let findTests() =
     let rxMode = System.Text.RegularExpressions.Regex("//\s*Mode:\s*([A-Za-z, ]*)")
     let rxPrelude = System.Text.RegularExpressions.Regex("//\s*Prelude:\s*([A-Za-z]*)")
@@ -101,22 +149,14 @@ let findTests() =
             let prelude = if rxPrelude.Match(content).Groups.Count > 1 then rxPrelude.Match(content).Groups.[1].Value.ToLower() else "true"
             let prelude = if prelude = "true" then true else false
             let desc = if rxDesc.Match(content).Groups.Count > 1 then rxDesc.Match(content).Groups.[1].Value else fname
-            if mode.Contains "values" && mode.Contains "types" then
-                [
-                    desc + ", test types", fun () -> testTypes prelude fname
-                    desc + ", test value", fun () -> testValues prelude fname
-                ]
-            else if mode.Contains "values" then
-                [
-                    desc, fun () -> testValues prelude fname
-                ]
-            else if mode.Contains "types" then
-                [
-                    desc, fun () -> testTypes prelude fname
-                ]
-            else
+            let res = []
+            let res = if mode.Contains "values"   then ($"{desc}, values", fun () -> testValues prelude fname)::res else res
+            let res = if mode.Contains "types"    then ($"{desc}, types", fun () -> testTypes prelude fname)::res  else res
+            let res = if mode.Contains "compiled" then ($"{desc}, compiled", fun () -> testJS prelude fname)::res     else res
+            if List.isEmpty res then
                 failwith <| sprintf "Invalid test mode '%s'." mode
-            )
+            else
+                res)
         |> Seq.toList
     ("Prelude types match", testPrelude) :: tests
 
@@ -126,12 +166,12 @@ let startTests() =
         match body() with
         | Ok () ->
             Console.ForegroundColor <- ConsoleColor.Green
-            let res = sprintf "| %-40s | Pass" name
+            let res = sprintf "| %-50s | Pass" name
             printfn "%s" res
             Ok res
         | Error err ->
             Console.ForegroundColor <- ConsoleColor.Red
-            let res = sprintf "| %-40s | Fail\n\t%s" name err
+            let res = sprintf "| %-50s | Fail\n\t%s" name err
             printfn "%s" res
             Error res) (findTests())
     Console.ResetColor()
